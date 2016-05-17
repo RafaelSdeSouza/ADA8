a4cc0a0525c39bdd553abf6189b17336d7b2cb35
Rafael S. de Souza <rafael.2706@gmail.com>
2016-05-17 18:42:25 +0200

    two normal examples

--- Normal/normal_linear_2.R
+++ Normal/normal_linear_2.R
@@ -5,18 +5,21 @@
 
 
 # Example of frequentist linear regression in R
-# 1 response (y) and 1 explanatory variable (x1)
+# 1 response (y) and 1 explanatory variable (x1) with a quadratic term
 
 
 set.seed(1056)                    # set seed to replicate example
-nobs= 100                        # number of obs in model 
-x1 <- runif(nobs)                 # random uniform variable
-mu <- 1 + 3*x1                    # linear predictor, xb
-y <- rnorm(nobs, mu, sd=0.25)     # create y as adjusted random normal variate 
+nobs= 150                         # number of obs in model 
+x1 <- runif(nobs,0,5)             # random uniform variable
+mu <- 1 + 5*x1-0.75*x1^2          # linear predictor, xb
+y <- rnorm(nobs, mu, sd=0.5)     # create y as adjusted random normal variate 
 
 # Visualize data
 
-fit <- lm(y ~ x1)               # Normla Fit of the synthetic data. 
+plot(x1,y)
+
+
+fit <- lm(y ~ x1+I(x1^2))               # Normla Fit of the synthetic data. 
 summary(fit) 
 
 xx <- seq(0,5,length=200)
@@ -25,5 +28,5 @@ ypred <- predict(fit,newdata=list(x1=xx),type="response") # Prediction from the 
 # Plot regression line 
 plot(x1,y,pch=19,col="red")
 lines(xx,ypred,col='cyan',lwd=4,lty=2)
-# Add the residuals
+# add the residuals
 segments(x1,fitted(fit),x1,y,lwd=2,col="gray")
