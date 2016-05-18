16d10073854b09c1aced705495dd546fb2c47957
Emille Ishida <emilleishida@gmail.com>
2016-05-19 00:01:13 +0200

    fix documentation for gaussian models

---
+++ Normal/README.md
@@ -0,0 +1,6 @@
+# Gaussian Models #
+
+Example scripts for Gaussian models. 
+
+* Normal linear regression in R  [1 explanatory variable](https://github.com/RafaelSdeSouza/ADA8/blob/master/Normal/normal_x1.R)  
+                                 [2 explanatory variables](https://github.com/RafaelSdeSouza/ADA8/blob/master/Normal/normal_x1_x2.R)
--- Normal/normal_x1.R
+++ Normal/normal_x1.R
@@ -3,27 +3,26 @@
 # Partial example from Bayesian Models for Astrophysical Data 
 # by Hilbe, de Souza & Ishida, 2016, Cambridge Univ. Press
 
-
 # Example of frequentist linear regression in R
+# synthetic data
 # 1 response (y) and 1 explanatory variable (x1)
 
 
 set.seed(1056)                    # set seed to replicate example
-nobs= 100                        # number of obs in model 
+nobs= 100                         # number of observations 
 x1 <- runif(nobs)                 # random uniform variable
-mu <- 1 + 3*x1                    # linear predictor, xb
+mu <- 1 + 3*x1                    # linear predictor
 y <- rnorm(nobs, mu, sd=0.25)     # create y as adjusted random normal variate 
 
-# Visualize data
-
-fit <- lm(y ~ x1)               # Normla Fit of the synthetic data. 
+# Fit model
+fit <- lm(y ~ x1)                 # Normal Fit of the synthetic data. 
 summary(fit) 
 
 xx <- seq(0,5,length=200)
 ypred <- predict(fit,newdata=list(x1=xx),type="response") # Prediction from the model 
 
-# Plot regression line 
-plot(x1,y,pch=19,col="red")
+ 
+plot(x1,y,pch=19,col="red")                               # Plot regression line
 lines(xx,ypred,col='cyan',lwd=4,lty=2)
-# Add the residuals
-segments(x1,fitted(fit),x1,y,lwd=2,col="gray")
+
+segments(x1,fitted(fit),x1,y,lwd=2,col="gray")            # Add the residuals
