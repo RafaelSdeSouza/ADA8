5f710a0df40932f6d9b28949debb6b51f3d6274a
Emille Ishida <emilleishida@gmail.com>
2016-05-19 03:21:07 +0200

    updated poisson

--- Count_models/Poisson/Ex1_Pois.R
+++ Count_models/Poisson/Ex1_Pois.R
@@ -1,19 +1,26 @@
+# ADA8 – Astronomical Data Analysis Summer School
+# Bayesian tutorial by Rafael S. de Souza - ELTE, Hungary & COIN
+#
+# Partial example from Bayesian Models for Astrophysical Data 
+# by Hilbe, de Souza & Ishida, 2016, Cambridge Univ. Press
+#
+# Example of Bayesian Poisson regression in R using JAGS
+# synthetic data
+# 1 response (y) and 1 explanatory variable (x1) 
+
+
 require(R2jags)
 require(ggplot2)
 source("https://raw.githubusercontent.com/RafaelSdeSouza/ADA8/master/Auxiliar_functions/jagsresults.R")
 
 
-set.seed(2016)
-nobs <- 500
-x1 <- runif(nobs)
-xb <- 1 + 2*x1
-py <- rpois(nobs, exp(xb)) 
+set.seed(2016)                                        # set seed to replicate example
+nobs <- 500                                           # number of observations
+x1 <- runif(nobs)                                     # random uniform variable
+xb <- 1 + 2*x1                                        # linear predictor
+py <- rpois(nobs, exp(xb))                            # create py as adjusted random normal variate
 poismod <- data.frame(py,x1)
 
-require(R2jags)
-
-X <- model.matrix(~ x1, data = poismod)
-K <- ncol(X)
 
 # Prepare data for prediction 
 M=1000
@@ -21,31 +28,39 @@ xx = seq(from =  min(x1),
          to =  max(x1), 
          length.out = M)
 
+# prepare data for JAGS input
+X <- model.matrix(~ x1, data = poismod)
+K <- ncol(X)
 
 POIS.data <- list(Y = py,
                   X = X,       
-                  K = K,         #Number of betas
+                  K = K,           #Number of betas
                   N = nobs,
                   xx = xx, 
-                  M = M) #Sample size
+                  M = M)           #Sample size
 
 
+# JAGS model
 Pois<-"model{
-  for (i in 1:K) {beta[i] ~ dnorm(0, 1e-4)}
-  for (i in 1:N) {
-    Y[i] ~ dpois(mu[i])  
-    log(mu[i]) <- inprod(beta[], X[i,])  
-  }
-# Prediction for new data
-for (j in 1:M){
-log(mux[j])<-beta[1]+beta[2]*xx[j]
-Yx[j]~dpois(mux[j])
-}
- 
-  }"
+  
+    for (i in 1:K) {beta[i] ~ dnorm(0, 1e-4)}               # non-informative prior for coefficients
+    for (i in 1:N) {
+        Y[i] ~ dpois(mu[i])                                 # likelihood function
+        log(mu[i]) <- inprod(beta[], X[i,])  
+    }
 
+    # Prediction for new data
+    for (j in 1:M){
+        log(mux[j])<-beta[1]+beta[2]*xx[j]
+        Yx[j]~dpois(mux[j])
+    }
+}"
+
+# set initial values
 inits  <- function () {
-  list(beta = rnorm(K, 0, 0.1))}
+    list(beta = rnorm(K, 0, 0.1))}
+
+# set parameter for prediction
 params <- c("Yx")
 
 P1 <-   jags(data       = POIS.data,
@@ -56,14 +71,14 @@ P1 <-   jags(data       = POIS.data,
              n.chains   = 3,
              n.burnin   = 2500,
              n.iter     = 5000)
-print(P1, intervals=c(0.025, 0.975), digits=3)
 
+# check results
+print(P1, intervals=c(0.025, 0.975), digits=3)
 
 # Plot
 yx <- jagsresults(x=P1, params=c('Yx'))
-gdata <- data.frame(x =xx, mean = yx[,"mean"],lwr1=yx[,"25%"],lwr2=yx[,"2.5%"],upr1=yx[,"75%"],upr2=yx[,"97.5%"])
-# 
 
+gdata <- data.frame(x =xx, mean = yx[,"mean"],lwr1=yx[,"25%"],lwr2=yx[,"2.5%"],upr1=yx[,"75%"],upr2=yx[,"97.5%"])
 
 ggplot(poismod ,aes(x=x1,y=py))+ 
   geom_point(colour="blue3",size=1,alpha=0.9)+
--- Count_models/README.md
+++ Count_models/README.md
@@ -1,3 +1,6 @@
 # Count models #
 
-* 
+* Negative Binomial model in R using JAGS  
+        - [1 explanatory variable](https://github.com/RafaelSdeSouza/ADA8/blob/master/Count_models/Negative_binomial/Ex1_NB.R)
+
+
