eb0050d84c3adb4b8d6de7298513506573ee95a8
Rafael <rafael.2706@gmail.com>
2016-05-19 03:25:12 +0200

    nb

--- Count_models/Negative_binomial/Ex1_NB.R
+++ Count_models/Negative_binomial/Ex1_NB.R
@@ -1,14 +1,22 @@
-library(R2jags)
 set.seed(141)
+library(R2jags)
 library(MASS)
-nobs <- 1500
-x1 <- rbinom(nobs,size=1, prob = 0.6)
-xb <- 1 + 2.5*x1
-theta <- 0.5
+library(scales)
+source("https://raw.githubusercontent.com/RafaelSdeSouza/ADA8/master/Auxiliar_functions/jagsresults.R")
+nobs <- 500
+x1 <- runif(nobs,0,4)
+xb <- -1 + 2.5*x1
+theta <- 0.75
 exb <- exp(xb)
 nby <- rnegbin(n = nobs, mu = exb, theta = theta)
 negbml <-data.frame(nby, x1)
 
+
+# Prepare data for prediction 
+M=500
+xx = seq(from = 0.95*min(x1), 
+         to = 1.05*max(x1), 
+         length.out = M)
 
 X <- model.matrix(~ x1, data=negbml)
 K <- ncol(X)
@@ -17,35 +25,84 @@ NB.data <- list(
   Y = negbml$nby,
   X = X,
   K = K,
-  N = nrow(negbml))
+  N = nobs,
+  M = M,
+  xx = xx)
+
+
 sink("NBGLM.txt")
 cat(" model{
     # Priors for coefficients
     for (i in 1:K) { beta[i] ~ dnorm(0, 0.0001)}
     # Prior for dispersion
-    theta ~ dunif(0.001, 5)
+    theta ~ dgamma(1e-3,1e-3)
     # Likelihood function
     for (i in 1:N){
     Y[i] ~ dnegbin(p[i], theta)
     p[i] <- theta/(theta + mu[i])
-    log(mu[i]) <- eta[i]
+    mu[i] <- exp(eta[i])
     eta[i] <- inprod(beta[], X[i,])
-    } }
+    }
+
+  # Prediction
+    for (j in 1:M){
+    etax[j] <- beta[1]+beta[2]*xx[j]
+    px[j] <- theta/(theta + mux[j])
+    mux[j] <- exp(etax[j])
+    Yx[j] ~ dnegbin(px[j], theta)
+   }
+
+
+}
     ",fill = TRUE)
 sink()
 inits <- function () { list(
   beta = rnorm(K, 0, 0.1), # Regression parameters
   theta = runif(0.00, 5) )
 }
-params <- c("beta", "theta")
+params <- c("beta", "theta","Yx")
 NB2 <- jags(data = NB.data, 
             inits = inits,
             parameters = params,
             model = "NBGLM.txt",
             n.thin = 1,
             n.chains = 3,
-            n.burnin = 3000,
+            n.burnin = 2500,
             n.iter = 5000)
             
 print(NB2, intervals=c(0.025, 0.975), digits=3)
+
+
+# Plot
+yx <- jagsresults(x=NB2, params=c('Yx'))
+
+
+NBdata <- data.frame(x1,nby)
+gdata <- data.frame(x =xx, mean = yx[,"50%"],lwr1=yx[,"25%"],lwr2=yx[,"2.5%"],upr1=yx[,"75%"],upr2=yx[,"97.5%"])
+
+
+asinh_trans <- function(){
+  trans_new(name = 'asinh', transform = function(x) asinh(x), 
+            inverse = function(x) sinh(x))
+}
+
+ggplot(NBdata,aes(x=x1,y=nby))+ geom_point(colour="green",size=1,alpha=0.35)+
+  geom_point(size=1.75,colour="green3")+
+  geom_ribbon(data=gdata,aes(x=xx,ymin=lwr1, ymax=upr1,y=NULL), alpha=0.3, fill=c("red"),show.legend=FALSE) +
+  geom_ribbon(data=gdata,aes(x=xx,ymin=lwr2, ymax=upr2,y=NULL), alpha=0.2, fill = c("orange"),show.legend=FALSE) +
+  geom_line(data=gdata,aes(x=xx,y=mean),colour="gray25",linetype="dashed",size=1,show.legend=FALSE)+
+  theme_bw()+
+  scale_y_continuous(trans = 'asinh',breaks=c(0,10,100,1000,10000,100000),
+  labels=c("0",expression(10^1),expression(10^2),expression(10^3),expression(10^4),expression(10^5)))
+
+
+
+
+
+
+
+
+
+
+
 
