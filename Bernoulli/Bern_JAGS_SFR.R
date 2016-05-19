c2bc0b34e390939af323b7ddfe300c121101ad87
Rafael S. de Souza <rafael.2706@gmail.com>
2016-05-19 21:47:07 +0200

    jags

--- Bernoulli/Bern_JAGS_SFR.R
+++ Bernoulli/Bern_JAGS_SFR.R
@@ -12,21 +12,11 @@ require(R2jags)
 require(ggplot2)
 source("https://raw.githubusercontent.com/RafaelSdeSouza/ADA8/master/Auxiliar_functions/jagsresults.R")
 
+SFR_dat<-read.csv("https://raw.githubusercontent.com/RafaelSdeSouza/ADA8/master/Bernoulli/SFR.csv")
 
-set.seed(1056)                           # set seed to replicate example
-nobs= 1000                               # number of obs in model 
-x1 <- runif(nobs,0,3)                    # random uniform variable
-eta <- -6 + 3.5*x1                       # linear predictor
-p <- 1/(1+exp(-eta))                     # inverse-logit link
-by <- rbinom(nobs,size=1, prob = p)      # create y as adjusted random bernoulli variate
-
-
-dat3 <- Biffi_data[,c("Xmol","SFR")]
-x <- dat3$Xmol
-y <- dat3$SFR 
-
-plot(x,y)
-
+x1 <- log(SFR_dat$Xmol,10)                   
+by <- SFR_dat$SFR    
+nobs <- nrow(SFR_dat)
 
 # Prepare data for prediction 
 M=500
@@ -37,14 +27,16 @@ xx = seq(from =  min(x1),
 
 # Construct data dictionary
 logitmod <-data.frame(by, x1)
-X <- model.matrix(~ 1+x1, 
+X <- model.matrix(~ 1+x1+I(x1^2)+I(x1^3), 
                   data = logitmod)
+
+XX <- model.matrix(~1+xx+I(xx^2)+I(xx^3))
 K <- ncol(X)
 logit_data <- list(Y  = logitmod$by, # Response variable
                    X  = X,           # Predictors
                    K  = K,           # Number of Predictors including the intercept 
                    N  = nobs,        # Sample size 
-                   xx = xx, 
+                   XX = XX, 
                    M = M
 )
 
@@ -58,12 +50,12 @@ for (i in 1:K) { beta[i] ~ dnorm(0, 0.0001) }
 for (i in 1:N){  
 Y[i] ~ dbern(p[i])
 logit(p[i]) <- eta[i]
-eta[i]  <- beta[1]+beta[2]*X[i,2]
+eta[i]  <- inprod(beta[], X[i,])
 }
 
 # Prediction for new data
 for (j in 1:M){
-etax[j]<-beta[1]+beta[2]*xx[j]
+etax[j]<-inprod(beta[], XX[j,])
 logit(px[j]) <- etax[j]
 Yx[j]~dbern(px[j])
 }
@@ -84,13 +76,13 @@ jagsfit<- jags(data       = logit_data,
                model      = textConnection(LOGIT),
                n.thin     = 1,
                n.chains   = 3,
-               n.burnin   = 3000,
-               n.iter     = 5000)
+               n.burnin   = 2500,
+               n.iter     = 8000)
 
 
 # check results
-print(jagsfit,intervals=c(0.025, 0.975),justify = "left", digits=2)
 
+jagsresults(x=jagsfit, params=c("beta"))
 
 # Plot
 y <- jagsresults(x=jagsfit, params=c('px'))
@@ -98,14 +90,14 @@ x <- xx
 gdata <- data.frame(x =xx, mean = y[,"mean"],lwr1=y[,"25%"],lwr2=y[,"2.5%"],upr1=y[,"75%"],upr2=y[,"97.5%"])
 
 # Bin data for visualization
-binx<-0.25
-t.breaks <-cut(x1, seq(0,3, by=binx))
+binx<-0.5
+t.breaks <-cut(x1, seq(min(x1),max(x1), by=binx))
 means <-tapply(by, t.breaks, mean)
 semean <-function(x) sd(x)/sqrt(length(x))
 means.se <-tapply(by, t.breaks, semean)
 
 
-gbin<-data.frame(x=seq(binx,3, by=binx),y=means)
+gbin<-data.frame(x=seq(binx+min(x1),max(x1), by=binx),y=means)
 
 ggplot(logitmod,aes(x=x1,y=by))+ 
   geom_point(colour="red",size=1,alpha=0.85,position = position_jitter (h = 0.075))+
@@ -115,6 +107,6 @@ ggplot(logitmod,aes(x=x1,y=by))+
   geom_ribbon(data=gdata,aes(x=xx,ymin=lwr1, ymax=upr1,y=NULL), alpha=0.45, fill=c("orange2"),show.legend=FALSE) +
   geom_ribbon(data=gdata,aes(x=xx,ymin=lwr2, ymax=upr2,y=NULL), alpha=0.35, fill = c("orange"),show.legend=FALSE) +
   geom_line(data=gdata,aes(x=xx,y=mean),colour="gray25",linetype="dashed",size=1,show.legend=FALSE)+
-  theme_bw() 
+  theme_bw()+coord_cartesian(ylim=c(0,1))+xlab(expression(log~x[mol]))+ylab("Probability of star formation activity")
 
 
